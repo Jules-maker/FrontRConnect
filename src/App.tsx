a0d7097699dd66ec13b732b52c6e6e847e77f4f8
Johann six <six.johann@gmail.com>
2024-02-06 14:13:51 +0100

    🎨 show how to use getGeo and getDistance

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
 import './App.css'
+import getGeolocalisation from './utils/getGeolocalisation'
+import getDistanceBetweenCords from './utils/getDistanceBetweenCords'
 
 function App() {
+  const [dist, setDist] = useState(0)
   
+
+  const handleClick = async() => {
+    try{
+
+      const geo = await getGeolocalisation();
+      console.log(geo);
+
+      const {latitude, longitude} = geo.coords;
+      const distance = getDistanceBetweenCords(latitude, longitude, 0, 0);
+      setDist(distance);
+      
+    } catch (e) {
+      console.error(e);
+    }
+  }
   return (
     <>
       <h1 className='bold'>R-connect</h1>
@@ -9,6 +28,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>Distance {dist}</p>
+        <button onClick={handleClick}>get Geo</button>
       </div>
     </>
   )
