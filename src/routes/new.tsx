ee62b8b2bfac571ea9f81077decc1f11b4a8d311
Johann six <six.johann@gmail.com>
2024-02-08 13:01:08 +0100

    🚧 add new page

---
+++ src/routes/new.tsx
@@ -0,0 +1,21 @@
+import ButtonComponent from "../components/ButtonComponent";
+import { setVisitedWebSite } from "../utils/vistiedWebSite";
+
+
+const New = () => {
+    return (
+        <div>
+        <h1>New</h1>
+        <ButtonComponent label="Visite" handleClick={() => {
+            console.log('Visite');
+            setVisitedWebSite();
+        }} />
+        <ButtonComponent label="Retour" handleClick={() => {
+            console.log('Retour');
+            window.history.back();
+        }} />
+        </div>
+    );
+    }
+
+export default New;
