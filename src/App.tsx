c434e1c04472841f66fb4a031afdfb01287e2ceb
Johann six <six.johann@gmail.com>
2024-02-08 10:57:35 +0100

    🚧 Add ListView component and update CardsComponent

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,29 @@ import getGeolocalisation from './utils/getGeolocalisation'
 import getDistanceBetweenCords from './utils/getDistanceBetweenCords'
 import reactLogo from './assets/react.svg'
 import ButtonComponent from './components/buttonComponent'
-import Cards from './components/CardsComponent'
+import Cards, { CardsProps } from './components/CardsComponent'
+import ListView from './components/ListComponent'
+
+const cardsList:CardsProps[]  = [
+  {
+    title: 'Card 1',
+    to: '#',
+    imgSrc: 'https://source.unsplash.com/random/320x320',
+    className: 'w-32 h-32'
+  },
+  {
+    title: 'Card 2',
+    to: '#',
+    imgSrc: 'https://source.unsplash.com/random/320x320',
+    className: 'w-32 h-32'
+  },
+  {
+    title: 'Card 3',
+    to: '#',
+    imgSrc: 'https://source.unsplash.com/random/320x320',
+    className: 'w-32 h-32'
+  }
+];
 
 function App() {
   const [dist, setDist] = useState(0)
@@ -34,6 +56,7 @@ function App() {
         <p>Distance {dist}</p>
         <ButtonComponent label="Get Geo" handleClick={handleClick} icon={<img src={reactLogo} className="h-5 w-5" alt="React logo" />} />
         <Cards title="Card 1" imgSrc="https://source.unsplash.com/random/320x320" className='w-32 h-32' to='#'/>
+        <ListView items={cardsList} renderItem={(item) => <Cards {...item} />} className='grid-cols-2'/>
       </div>
     </>
   )
