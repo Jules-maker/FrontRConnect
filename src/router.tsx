db3154adc3adba4d0aa849c6b1f86ddb36c09c21
Johann six <six.johann@gmail.com>
2024-02-08 13:00:56 +0100

    ✨ Add new route and redirect first-time visitors to /new

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 import App from './App.tsx'
 import Root from "./routes/root.tsx";
 import ErrorPage from './error-page.tsx';
+import New from './routes/new.tsx';
+import { isFirstVisite } from './utils/vistiedWebSite.ts';
 
 // Add here the page to import and the route to use
 const router = createBrowserRouter([
@@ -9,11 +11,22 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: async () => {
+      // check if it's the first visite
+      if (isFirstVisite()) {
+        return redirect('/new');
+      }
+      return null;
+    }
   },
   {
     path: "/app",
     element: <App />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "/new",
+    element: <New />,
   }
 ]);
 
