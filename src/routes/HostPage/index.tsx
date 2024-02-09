bc66ab1e4e7f4708d0d9eb17c8ee28559d28a83f
Johann six <six.johann@gmail.com>
2024-02-09 11:55:46 +0100

    🚧 test router loader

--- src/routes/HostPage/index.tsx
+++ src/routes/HostPage/index.tsx
@@ -1,11 +1,13 @@
+import { useLoaderData } from "react-router-dom";
 
 const HostPage = () => {
+    const { id } = useLoaderData() as { id: number };
     return (
         <div>
         <h1>Host</h1>
-       
+        {id && <p>{id} </p>}
         </div>
     );
-    }
+}
 
 export default HostPage;
