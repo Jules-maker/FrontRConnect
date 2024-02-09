02c1721a6d523e7425354582a69cde495eba626b
Johann six <six.johann@gmail.com>
2024-02-09 15:27:39 +0100

    ♻️ refacto homePage component and add search functionality

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,58 +1,28 @@
 import { useState } from 'react'
 import Cards, { CardsProps } from '../components/CardsComponent';
-import getGeolocalisation from '../utils/getGeolocalisation';
-import getDistanceBetweenCords from '../utils/getDistanceBetweenCords';
-import ButtonComponent from '../components/ButtonComponent';
+import HeaderComponent from '../components/HeaderComponent';
+import SearchBarComponent from '../components/SearchBarComponent';
 import ListView from '../components/ListComponent';
-import reactLogo from '../assets/react.svg';
+import { useLoaderData } from 'react-router-dom';
 
-const cardsList:CardsProps[]  = [
-  {
-    title: 'Card 1',
-    to: '/host/1',
-    imgSrc: 'https://source.unsplash.com/random/320x320',
-    className: 'w-32 h-32'
-  },
-  {
-    title: 'Card 2',
-    to: '/host/2',
-    imgSrc: 'https://source.unsplash.com/random/320x320',
-    className: 'w-32 h-32'
-  },
-  {
-    title: 'Card 3',
-    to: '/host/3',
-    imgSrc: 'https://source.unsplash.com/random/320x320',
-    className: 'w-32 h-32'
-  }
-];
 
 function HomePage() {
-  const [dist, setDist] = useState(0)
-  
+  const { cardsList } = useLoaderData() as { cardsList: CardsProps[] }; 
 
-  const handleClick = async() => {
-    try{
+  const [filteredData, setFilteredData] = useState<CardsProps[]>(cardsList);
 
-      const geo = await getGeolocalisation();
-      console.log(geo);
 
-      const {latitude, longitude} = geo.coords;
-      const distance = getDistanceBetweenCords(latitude, longitude, 0, 0);
-      setDist(distance);
-      
-    } catch (e) {
-      console.error(e);
-    }
-  }
+    const handleSearch = (filteredData: CardsProps[]) => {
+      setFilteredData(filteredData);
+    };
   return (
     <>
-      <h1 className='bold'>R-connect</h1>
-      <div className="card">
-        <p>Distance {dist}</p>
-        <ButtonComponent label="Get Geo" handleClick={handleClick} icon={<img src={reactLogo} className="h-5 w-5" alt="React logo" />} />
-        <Cards title="Card 1" imgSrc="https://source.unsplash.com/random/320x320" className='w-32 h-32' to='#'/>
-        <ListView items={cardsList} renderItem={(item) => <Cards {...item} />} className='grid-cols-2'/>
+      <HeaderComponent  title='R-Connect' />
+      <div className='p-4 flex flex-col gap-3'>
+        <SearchBarComponent data={cardsList} onSearch={handleSearch} itemToFilter={'title'}/>
+        <h2 className='bold text-black dark:text-white text-xl'>Nouveau Lieu</h2>
+        <ListView items={filteredData} renderItem={(item) => <Cards {...item} className='w-full rounded-md'/>} className='grid-cols-2 gap-4 overflow-auto'/>
+
       </div>
     </>
   )
