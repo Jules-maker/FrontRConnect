b2d4eb64cdd436a73cf638e239057f4646542c5d
Johann six <six.johann@gmail.com>
2024-02-09 15:27:19 +0100

    🧐 data example

---
+++ src/data.ts
@@ -0,0 +1,25 @@
+import { CardsProps } from "./components/CardsComponent";
+
+const cardsList:CardsProps[]  = [
+    {
+      title: 'Card 1',
+      to: '/host/1',
+      imgSrc: 'https://source.unsplash.com/random/320x320',
+      className: 'w-32 h-32'
+    },
+    {
+      title: 'Resto',
+      to: '/host/2',
+      imgSrc: 'https://source.unsplash.com/random/320x320',
+      className: 'w-32 h-32'
+    },
+    {
+      title: 'Bla',
+      to: '/host/3',
+      imgSrc: 'https://source.unsplash.com/random/320x320',
+      className: 'w-32 h-32'
+    }
+];
+
+
+export default cardsList;
